654d2525543169408951e1dafa0162e7e3f5f957
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-19 10:38:09 +0900

    Skip transpilation of CommonJS modules

--- test/basic.test.js
+++ test/basic.test.js
@@ -10,6 +10,13 @@ describe(`basic babel usage`, () => {
     });
   });
 
+  it(`allows loading CJS modules`, async () => {
+    const example = await import("./fixtures/basic/cjs.cjs");
+    assert.deepEqual(example.default, {
+      cjs: "cjs",
+    });
+  });
+
   it(`supports ES module babel config files`, async () => {
     const mjsConfig = await import("./fixtures/mjs-config/main.js");
     assert.deepEqual(mjsConfig.default, {
