2e1fc9408f1dc3bc01395faa9ca9e5ec1fb8fb54
joeldenning <joeldenning@gmail.com>
2024-01-09 11:59:25 -0700

    Support for preset-typescript, .ts, and .tsx files

--- test/run-tests.js
+++ test/run-tests.js
@@ -4,6 +4,7 @@ const mocha = new Mocha();
 
 mocha.addFile("./test/basic.test.js");
 mocha.addFile("./test/commonjs.test.js");
+mocha.addFile("./test/typescript.test.js");
 
 mocha.loadFilesAsync().then(() => {
   mocha.run();
